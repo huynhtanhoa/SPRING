d0023bd20f3cbd288e3f52991941c0b6a8f92e59
steven <huynhtanhoa2012@gmail.com>
2024-05-14 18:17:48 +0700

    Consuming a RESTful Web Service
    Spring’s RestTemplate to retrieve a random Spring Boot quotation at http://localhost:8080/api/random.

---
+++ consuming-rest/src/main/java/com/example/consumingrest/ConsumingRestApplication.java
@@ -0,0 +1,14 @@
+package com.example.consumingrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumingRestApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ConsumingRestApplication.class, args);
+	}
+
+}
