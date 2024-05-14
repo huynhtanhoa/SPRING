e1340230e13ac4a68eeb1be3276f8c7cdcb53f1d
steven <huynhtanhoa2012@gmail.com>
2024-05-14 18:28:02 +0700

    developed a simple REST client by using Spring Boot.

---
+++ consuming-rest/src/main/java/com/example/consumingrest/Quote.java
@@ -0,0 +1,7 @@
+package com.example.consumingrest;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record Quote(String type, Value value)  {
+}
