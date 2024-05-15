247572a5d36d0e6c920f4ea8dcedd44324931826
steven <huynhtanhoa2012@gmail.com>
2024-05-15 12:46:03 +0700

    Uploading Files
    creating a server application that can receive HTTP multi-part file uploads.

---
+++ uploading-files/src/main/java/com/example/uploadingfiles/FileUploadController.java
@@ -0,0 +1,17 @@
+package com.example.uploadingfiles;
+
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class FileUploadController {
+
+    private final StorageService storageService;
+
+    public 
+
+
+
+
+
+}
